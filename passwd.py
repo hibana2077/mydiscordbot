260c1f3412de3b40c63a5a9a0a8095ef5fe410dc
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-04-13 01:51:14 +0800

    Add files via upload

---
+++ passwd.py
@@ -0,0 +1,16 @@
+'''
+Author: hibana2077
+Date: 2022-04-09 21:07:53
+LastEditTime: 2022-04-13 01:29:23
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \HELP\passwd.py
+'''
+API_KEY="ZfPdO0K6PONcxTyFSawpMkr8HkZFdI5aBBC1tmzPw8j2Tz89V2uCkC7zYMYswYvm"#你的幣安API_KEY
+API_SECRET="mZKX4SydJlEaq3NgPyKypbZWvxprzts2LB6eVeUDfXMLz490kvtcpRNmJO55dfgP"#你的幣安API_SECRET
+my_opendataAPI="a5e87e8e-043a-4724-98e6-4bc3e547e83b"#國家機器API
+API_DISCORD_TOKEN="OTYyNjA1ODE0OTY5OTU0MzE2.YlJ-hg.FPezfAt7JRczM-JcN0Ozbh1AMeU"#你的discord token
+FTX_API_KEY="05Brpeejog61nkvJUaJb8JCkMWvtH_5sv0cmvUz2"#FTX
+FTX_API_SECRET="RECmRXLRdCFQWzMuOUNMUPrb1WBtj79zxCtKUmz2"#FTX
+TEST_API_KEY="86495836620b21e98cfa69affce9e14711667c26371f132581b10e8cd7b77f0e"#你的幣安TEST_API_KEY
+TEST_API_SECRET="d386988bd880f621e7199436b1773d9e07caf0352447030d550cf5ab4c04dae3"#你的幣安TEST_API_SECRET
