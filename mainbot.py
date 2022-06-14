846d3f91f2c97f66cf3b2bb93f23c610c1413932
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-06-14 17:54:57 +0800

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -29,7 +29,7 @@ async def on_message(message):
     #排除自己的訊息，避免陷入無限循環
     if message.author == client.user:
         return
-    if message.content.startswith('price'):
+    if message.content.startswith('price'):#獲取報價
         
         if len(message.content.split(" ")) == 2:
             url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + message.content.split(" ")[1].upper() + "USDT"
@@ -45,7 +45,7 @@ async def on_message(message):
            pos = response.text.index("lastPrice") + 12     
            await message.channel.send(require[i] + ":" + response.text[pos:pos+8])
            i = i + 1
-    if message.content.startswith('fprice'):
+    if message.content.startswith('fprice'):#獲取合約報價
         
         if len(message.content.split(" ")) == 2:
             url = "https://fapi.binance.com/fapi/v1/premiumIndex?symbol=" + message.content.split(" ")[1].upper() + "USDT"
@@ -61,14 +61,14 @@ async def on_message(message):
            pos = response.text.index("markPrice") + 12     
            await message.channel.send(require[i] + ":" + response.text[pos:pos+8])
            i = i + 1
-    if message.content.startswith('binprice'):
+    if message.content.startswith('binprice'):#獲取更完整爆價
         if len(message.content.split(" ")) == 2:
             price,coin=binclient.get_all_tickers(),message.content.split(" ")[1].upper()
             for t in price:
                 if t['symbol']==coin:await message.channel.send(message.content.split(" ")[1].upper() + "價格為:" + t['price']+"USDT")
         else:await message.channel.send('請輸入幣種名稱 EX:APEUSDT')
 
-    if message.content.startswith('bindeposit'):
+    if message.content.startswith('bindeposit'):#顯示目前帳號的地址 需要提供幣種
         if len(message.content.split(" ")) == 2:
             address = binclient.get_deposit_address(coin=message.content.split(" ")[1].upper())
             await message.channel.send(message.content.split(" ")[1].upper() + "存款地址為:" +address['address'])
@@ -82,52 +82,52 @@ async def on_message(message):
                     await message.channel.send(message.content.split(" ")[1].upper() + "存款地址為:" +str(wi[1]))
         else:await message.channel.send('沒有提款紀錄將不會顯示')
     
-    if message.content.startswith('MCserver'):
+    if message.content.startswith('MCserver'):#輸出林品豪架設的MC server的資訊
         await message.channel.send('https://web.nttu.edu.tw/s31/MinecraftServer/server.php')
 
-    if message.content.startswith('MCbuild'):
+    if message.content.startswith('MCbuild'):#一些影片
         await message.channel.send('https://youtu.be/uldhfEZydTo')
     
-    if message.content.startswith('TTweather'):
+    if message.content.startswith('TTweather'):#台東天氣
         r=requests.get('https://data.epa.gov.tw/api/v2/aqx_p_249?api_key=a5e87e8e-043a-4724-98e6-4bc3e547e83b', verify=False)
         for t in range(6):
             await message.channel.send(f"時間{datetime.date.today()}:{r.json()['records'][t]['sitename']}的{r.json()['records'][t]['itemengname']}為 {r.json()['records'][t]['concentration']} {r.json()['records'][t]['itemunit']}")
 
-    if message.content.startswith('date'):
+    if message.content.startswith('date'):#現在時間
         await message.channel.send(f"現在時間:{datetime.date.today()}")
 
     if message.content.startswith('講中文拉幹!'):
         await message.channel.send(f"我幹你娘機掰!沒被幹過是不是!")
 
-    if message.content.startswith('python Docs'):
+    if message.content.startswith('python Docs'):#顯示python docs
         await message.channel.send(f"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2048px-Python-logo-notext.svg.png")
         await message.channel.send(f"https://docs.python.org/3/")
 
-    if message.content.startswith('cpp Docs'):
+    if message.content.startswith('cpp Docs'):#顯示cpp docs
         await message.channel.send(f"https://pic.vsixhub.com/35/5d/b4f78906-078e-4ba3-88ef-02999a9bb4b7-logo.png")
         await message.channel.send(f"https://en.cppreference.com/w/")
 
-    if message.content.startswith('UVI'):
+    if message.content.startswith('UVI'):#顯示台灣各地的UV
         r=requests.get('https://data.epa.gov.tw/api/v2/uv_s_01?api_key=a5e87e8e-043a-4724-98e6-4bc3e547e83b', verify=False)
         for t in range(34):await message.channel.send(f"地點:{r.json()['records'][t]['sitename']} UV:{r.json()['records'][t]['uvi']} 時間:{r.json()['records'][t]['publishtime']}")
     
-    if message.content.startswith('binfuex') and message.content.split()[1]!="test":#非常重要的部分 考慮挖出來做?
+    if message.content.startswith('binfuex') and message.content.split()[1]!="test":#下單的部分
         try:
             qu=float(message.content.split()[3])
             order=binclient.futures_create_order(symbol=message.content.split()[1],side=message.content.split()[2].upper(),type="MARKET",quantity=int(qu))
             await message.channel.send(f"下單 {order['symbol']} 狀態{order['status']} 方向{order['side']} ")
         except Exception as e:await message.channel.send(e.message, "error")
     
-    if message.content.startswith('binasset'):
+    if message.content.startswith('binasset'):#查看剩下多少錢
         asetname=message.content.split()[1]
         await message.channel.send(f"資產項目:{binclient.get_asset_balance(asset=asetname)['asset']} 可用: {binclient.get_asset_balance(asset=asetname)['free']} 不可用: {binclient.get_asset_balance(asset=asetname)['locked']}")
 
-    if message.content.startswith('binleverange'):
+    if message.content.startswith('binleverange'):#調整槓桿
         coinname,lv=message.content.split()[1],int(message.content.split()[2])
         case=binclient.futures_change_leverage(symbol=coinname,leverage=lv)
         await message.channel.send(f"更改 {case['symbol']} 的槓桿為 {case['leverage']} 倍，最大名義值為 {case['maxNotionalValue']} USDT。")
     
-    if message.content.startswith('testbinorder'):
+    if message.content.startswith('testbinorder'):#測試下單用的
         try:
             qu=float(message.content.split()[3])
             countprofit.setorder(message.content.split()[4],message.content.split()[5],int(qu),float(message.content.split()[6]),message.content.split()[1],message.content.split()[2])
